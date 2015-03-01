f2487243a73c08963ecba0a292854d2d13a63dcd
Shlomo Twerski <ystwerski@gmail.com>
2015-03-01 11:19:21 -0600

    Added controller and started making restful actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301163022) do
+ActiveRecord::Schema.define(version: 20150301170132) do
+
+  create_table "links", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "slug",       limit: 255
+    t.text     "target_url", limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
